a1d4dc88e6adcfafbbbbf1fee64b6846f368b7da
Sergey Kubatko <skubatko@gmail.com>
2024-06-01 13:57:12 +0300

    hw08 / preliminary setup

---
+++ server/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.springframework.boot")
+    kotlin("plugin.spring")
+    kotlin("kapt")
+    application
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+dependencies {
+    implementation(project(":api"))
+
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+}
+
+springBoot {
+    buildInfo()
+}
+
+application {
+    mainClass.set("ru.skubatko.dev.server.ServerApplicationKt")
+}
---
+++ server/src/test/kotlin/ru/skubatko/dev/server/app/ServerApplicationSBTest.kt
@@ -0,0 +1,12 @@
+package ru.skubatko.dev.server.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationSBTest {
+
+    @Test
+    fun contextLoads() {
+    }
+}
